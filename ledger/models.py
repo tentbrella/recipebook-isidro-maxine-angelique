e1f6c28c49532cdbf88c1451fe4196f7b65aaff7
tentbrella <maxneisidro@gmail.com>
2025-03-06 16:42:00 +0800

    Updated models.py
    Added the string representation and absolute urls of the Ingredient and Recipe models

--- ledger/models.py
+++ ledger/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return '{}'.format(self.name)
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[str(self.name)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return '{}'.format(self.name)
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
