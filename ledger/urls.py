de61d1d3832088ec1a924ed9eb7a3e35f519c023
tentbrella <maxneisidro@gmail.com>
2025-03-07 22:03:59 +0800

    Edited recipe urls
    changed recipe urls so they direct to a working page

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,8 +5,8 @@ from .views import index, recipe_book, recipe1, recipe2
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', recipe_book, name="recipe-book"),
-    path('recipe/1', recipe1, name="recipe1"),
-    path('recipe/2', recipe2, name="recipe2"),
+    path('recipes/ledger/recipe/1', recipe1, name="recipe1"),
+    path('recipes/ledger/recipe/2', recipe2, name="recipe2"),
 ]
 
 app_name = "ledger"
