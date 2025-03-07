aa3bf4cd67bb7e90331e3e0566238330f74294c6
tentbrella <maxneisidro@gmail.com>
2025-03-07 21:31:18 +0800

    Repo Cleanup and Started Admin Setup
    Deleted pycache folders and registered models in admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
