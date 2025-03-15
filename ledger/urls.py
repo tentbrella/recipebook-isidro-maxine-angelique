9116a703757f3d12afa0b25bc56037b0116dada2
tentbrella <maxneisidro@gmail.com>
2025-03-15 10:20:13 +0800

    Added login and logout pages

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,4 +6,6 @@ app_name = "ledger"
 urlpatterns = [
     path('recipes/list', RecipeBookView.as_view(), name="recipe-book"),
     path('recipe/<int:pk>/', RecipeView.as_view(), name="recipe"),
+    path('accounts/login', include('django.contrib.auth.urls')),
+    path('accounts/logout', include('django.contrib.auth.urls')),
     ]
