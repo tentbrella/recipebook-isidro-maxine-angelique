f85b0b78f0ad6ef6f78b65e9a9ade87a4009b8c6
tentbrella <maxneisidro@gmail.com>
2025-03-15 09:53:49 +0800

    Created profile model, updated recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +10,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
         return '{}'.format(self.name)
     def get_absolute_url(self):
@@ -20,3 +24,8 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe')
     def __str__(self):
         return '{}: needs {} of {}'.format(self.recipe, self.quantity, self.ingredient)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
