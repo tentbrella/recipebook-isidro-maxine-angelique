7eb457a55b6e3252a438f5cf78c820f5e678fd12
tentbrella <maxneisidro@gmail.com>
2025-03-15 10:08:16 +0800

    Made RecipeView only visible to logged in users

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ledger.models import Ingredient, Recipe, RecipeIngredient
 from django.views.generic import ListView, DetailView
 
@@ -8,10 +9,11 @@ class RecipeBookView(ListView):
     template_name = 'list.html'
     context_object_name = 'recipes'
     
-class RecipeView(DetailView):
+class RecipeView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'detail.html'
     context_object_name = 'recipe'
+    redirect_field_name = '/ledger/recipes/list'
 
 def index(request):
     return HttpResponse('Hello World! This came from the index view')
